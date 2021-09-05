47e1bb0f45cb32a7d02f0b09ba5755c7bd03b300
Sean Blake <sean.blake97@outlook.com>
2021-09-04 22:12:40 -0400

    Initial Commit

---
+++ It Guhso/It_GuhsoApp.swift
@@ -0,0 +1,17 @@
+//
+//  It_GuhsoApp.swift
+//  It Guhso
+//
+//  Created by SEAN BLAKE on 9/4/21.
+//
+
+import SwiftUI
+
+@main
+struct It_GuhsoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
